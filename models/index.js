93a20ec122e4425175ab108efe8fa75ad6fb70db
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-04-28 19:11:14 -0700

    create find all category

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Products.belongsToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id'
 });
